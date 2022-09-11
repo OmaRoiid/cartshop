0a58771a02cfa7863497a9a6ef86ebbe7e7b870d
Omar Salem <omarsalem977@gmail.com>
2022-09-12 01:50:42 +0200

    add ui of cart

--- js/home.js
+++ js/home.js
@@ -1,5 +1,6 @@
 let products = [];
 let selectedProductToCart = [];
+let cartCounter = 0;
 
 (async function getProducts() {
   const itemCardsWrapper = document.getElementById("item_cards");
@@ -13,7 +14,7 @@ let selectedProductToCart = [];
     content += `
         <div  class="card" >
         <div class="card__price"><span> ${products[i].price}EGP</span></div>
-        <img class="card__image-container" src="${products[i].image}" alt="Card image cap" ;
+        <img class="card__image-container" src="${products[i].image}" alt="product image"
 " >
         <hr>
         <div class="card__content">
@@ -30,45 +31,44 @@ let selectedProductToCart = [];
           `;
   }
   itemCardsWrapper.innerHTML = content;
+  document.getElementById("cart_counter").innerHTML = cartCounter;
 })();
 
 function onAddToCart(selectedProduct) {
-  // debugger;
-  // let selected = products.find((product) => {
-  //   return product.id === selectedProduct;
-  // });
-  // let index = products.indexOf((product) => {
-  //   return product.id === selectedProduct;
-  // });
+  let selected = products.find((item) => item.id === selectedProduct);
+  selectedProductToCart.push(selected);
+  document.getElementById("cart_counter").innerHTML = ++cartCounter;
+  renderCartList();
+}
 
-  let selected = products.find((item, index) => {
-    if (item.id === selectedProduct) {
-      return {
-        ...item,
-        count: selectedProductToCart[index].count
-          ? selectedProductToCart[index].count
-          : 1,
-      };
-    }
-  });
-
-  console.log(newoooo);
-  // selected = {
-  //   ...selected,
-  //   count: selectedProductToCart[index].count
-  //     ? selectedProductToCart[index].count
-  //     : 1,
-  // };
-  let found = false;
-  selectedProductToCart = selectedProductToCart.map((item) => {
-    if (item.id === selected.id) {
-      found = true;
-      console.log({ ...item, count: ++selected.count });
-      return { ...item, count: ++selected.count };
-    }
-  });
-  if (!found) {
-    selectedProductToCart.push(selected);
+function renderCartList() {
+  const cartList = document.getElementById("cart__list");
+  let cartListContent = "";
+  for (let i = 0; i < selectedProductToCart.length; i++) {
+    cartListContent += `
+      <div class="cart__items">
+    <div class="cart__items--img">
+      <img src="${selectedProductToCart[i].image}" alt="product image" />
+    </div>
+ 
+    <div class="description">
+      <span>${selectedProductToCart[i].title}</span>
+      <span>${selectedProductToCart[i].price}</span>
+    </div>
+ 
+    <div class="cart__quantity">
+      <button class="plus-btn" type="button" name="button">
+        +
+      </button>
+      <input type="text" name="name" value="1">
+      <button class="minus-btn" type="button" name="button">
+        -
+      </button>
+    </div>
+   </div>
+          `;
   }
-  console.log(selectedProductToCart);
+  console.log(cartListContent);
+  cartList.innerHTML = cartListContent;
+  console.log(cartList);
 }
